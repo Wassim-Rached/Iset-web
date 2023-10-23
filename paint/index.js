fd2dd4655b9c0976563f889284dd82b59f9716a0
Wassim-Rached <wassimrached404@gmail.com>
2023-10-23 11:36:55 +0100

    addd

--- paint/index.js
+++ paint/index.js
@@ -1,7 +1,9 @@
 const moncanvas = document.getElementById("dessin");
 const ctx = moncanvas.getContext("2d");
+
 var en_dessin = false;
 var form = "creuse";
+var eraser_size = 20;
 
 const shapes_trash = [];
 const shapes = [];
@@ -9,27 +11,15 @@ const shapes = [];
 const line_btn__dom = document.getElementById("line-btn");
 const cercle_btn__dom = document.getElementById("cercle-btn");
 const rectangle_btn__dom = document.getElementById("rectangle-btn");
+const eraser_btn__dom = document.getElementById("eraser-btn");
 
-// Propriétés graphiques par défaut _
 ctx.strokeStyle = "black";
 ctx.lineWidth = 2;
 
-// Bouton de souris relâché _
 moncanvas.onmouseup = function (e) {
-  // Dessin désactivé
   en_dessin = false;
 };
 
-// Ajoute un segment au tracé _
-function dessiner(x, y) {
-  if (form == "creuse") {
-    ctx.stroke();
-  }
-
-  if (form == "pleine") {
-    ctx.fill();
-  }
-}
 function drawAllShapes() {
   ctx.clearRect(0, 0, moncanvas.width, moncanvas.height);
 
@@ -39,6 +29,7 @@ function drawAllShapes() {
 
     switch (shape.type) {
       case "line":
+        ctx.lineWidth = shape.lineWidth;
         if (shape.move_to_x) {
           ctx.beginPath();
           ctx.moveTo(shape.move_to_x, shape.move_to_y);
@@ -67,13 +58,16 @@ function drawAllShapes() {
         }
         break;
 
+      case "eraser":
+        ctx.clearRect(shape.x, shape.y, shape.size, shape.size);
+        break;
+
       default:
         break;
     }
   });
 }
 
-// Modification de la couleur du contexte _
 function modifierCouleur(codeCouleur) {
   if (codeCouleur) {
     ctx.strokeStyle = codeCouleur;
@@ -82,7 +76,7 @@ function modifierCouleur(codeCouleur) {
 }
 
 line_btn__dom.addEventListener("click", (event) => {
-  // Bouton de souris activé
+  reset_moncanvas_events();
 
   moncanvas.onmousedown = (e) => {
     en_dessin = true;
@@ -100,12 +94,12 @@ line_btn__dom.addEventListener("click", (event) => {
       fillStyle: ctx.fillStyle,
       move_to_x: move_to_x,
       move_to_y: move_to_y,
+      lineWidth: ctx.lineWidth,
     });
 
     drawAllShapes();
   };
 
-  // Mouvement de souris
   moncanvas.onmousemove = function (e) {
     if (en_dessin) {
       //   ctx.lineTo(e.offsetX, e.offsetY);
@@ -119,6 +113,7 @@ line_btn__dom.addEventListener("click", (event) => {
         fillStyle: ctx.fillStyle,
         move_to_x: null,
         move_to_y: null,
+        lineWidth: ctx.lineWidth,
       });
 
       drawAllShapes();
@@ -127,6 +122,8 @@ line_btn__dom.addEventListener("click", (event) => {
 });
 
 cercle_btn__dom.addEventListener("click", (event) => {
+  reset_moncanvas_events();
+
   let startX = 0;
   let startY = 0;
 
@@ -196,6 +193,8 @@ cercle_btn__dom.addEventListener("click", (event) => {
 });
 
 rectangle_btn__dom.addEventListener("click", (event) => {
+  reset_moncanvas_events();
+
   let startX = 0;
   let startY = 0;
 
@@ -255,6 +254,34 @@ rectangle_btn__dom.addEventListener("click", (event) => {
   };
 });
 
+eraser_btn__dom.addEventListener("click", (event) => {
+  reset_moncanvas_events();
+
+  moncanvas.onclick = (e) => {
+    const x = e.offsetX;
+    const y = e.offsetY;
+    shapes.push({ type: "eraser", x: x, y: y, size: eraser_size });
+    drawAllShapes();
+  };
+
+  moncanvas.onmousedown = (e) => {
+    en_dessin = true;
+  };
+
+  moncanvas.onmousemove = (e) => {
+    if (!en_dessin) return;
+
+    const x = e.offsetX;
+    const y = e.offsetY;
+    shapes.push({ type: "eraser", x: x, y: y, size: eraser_size });
+    drawAllShapes();
+  };
+
+  moncanvas.onmouseup = (e) => {
+    en_dessin = false;
+  };
+});
+
 function setForm(value) {
   form = value;
 }
@@ -270,3 +297,26 @@ function redo() {
   shapes.push(restored_shape);
   drawAllShapes();
 }
+
+document.getElementById("line_width").addEventListener("change", (e) => {
+  let value = parseInt(e.target.value);
+  if (value > 0) {
+    ctx.lineWidth = value;
+  }
+});
+
+document.getElementById("eraser_size").addEventListener("change", (e) => {
+  let value = parseInt(e.target.value);
+  if (value > 0) {
+    eraser_size = value;
+  }
+});
+
+function reset_moncanvas_events() {
+  moncanvas.onmousedown = null;
+  moncanvas.onmouseover = null;
+  moncanvas.onclick = null;
+}
+
+// default
+line_btn__dom.click();
